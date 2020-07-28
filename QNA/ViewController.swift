f15ccc37bca07376ceeed37e636abc0623b98520
lee <g7huway0310@gmail.com>
2020-07-28 10:20:36 +0800

    Initial Commit

---
+++ QNA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QNA
+//
+//  Created by guowei on 2020/7/28.
+//  Copyright © 2020 guowei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
